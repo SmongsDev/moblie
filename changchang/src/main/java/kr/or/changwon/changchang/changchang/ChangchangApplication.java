7658465b934dec3d5c554565867c9093a17db3ed
SmongsDev <sssm0928@naver.com>
2024-11-09 23:00:57 +0900

    MoblieProgramming Project

---
+++ changchang/src/main/java/kr/or/changwon/changchang/changchang/ChangchangApplication.java
@@ -0,0 +1,13 @@
+package kr.or.changwon.changchang.changchang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangchangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangchangApplication.class, args);
+	}
+
+}
---
+++ changchang/src/test/java/kr/or/changwon/changchang/changchang/ChangchangApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.changwon.changchang.changchang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangchangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
