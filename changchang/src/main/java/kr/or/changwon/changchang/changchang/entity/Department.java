7348de7f47f85957de8bcaff40db3e0c3f2eeb41
SmongsDev <sssm0928@naver.com>
2024-12-06 22:19:54 +0900

    data.sql 수정

--- changchang/src/main/java/kr/or/changwon/changchang/changchang/entity/Department.java
+++ changchang/src/main/java/kr/or/changwon/changchang/changchang/entity/Department.java
@@ -5,11 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.Data;
 
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+@Data
 @Entity
 public class Department {
 
@@ -23,45 +25,4 @@ public class Department {
 	@OneToMany(mappedBy = "department")
 	@JsonManagedReference // 순환 참조 문제 해결
 	private List<Notice> notices;
-
-	// Getters and Setters
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getCrawlUrl() {
-		return crawlUrl;
-	}
-
-	public void setCrawlUrl(String crawlUrl) {
-		this.crawlUrl = crawlUrl;
-	}
-
-	public String getLinkPattern() {
-		return linkPattern;
-	}
-
-	public void setLinkPattern(String linkPattern) {
-		this.linkPattern = linkPattern;
-	}
-
-	public List<Notice> getNotices() {
-		return notices;
-	}
-
-	public void setNotices(List<Notice> notices) {
-		this.notices = notices;
-	}
 }
